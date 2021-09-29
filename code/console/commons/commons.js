d749a10099d7afc4cc232ae1ac72e8dd0ef50d6a
Robert Kleniewski <robert.kleniewski@gmail.com>
2021-09-29 14:15:52 +0200

    test

--- code/console/commons/commons.js
+++ code/console/commons/commons.js
@@ -1,6 +1,6 @@
 exports.appcolor = process.env.APPCOLOR || 'brown';
 exports.appname  = process.env.APPNAME  || 'ISV Console';
 exports.appsdir  = process.env.APPSDIR  || 'apps/';
-exports.ocptoken  = process.env.OCPTOKEN  || 'sha256~0ITt6Xkgp8gqbJIxZQAspqhG5Q1m_u0zwxvdyNTTHsg';
-exports.ocpserver = process.env.OCPSERVER || 'https://c113-e.eu-de.containers.cloud.ibm.com:31739';
+exports.ocptoken  = process.env.OCPTOKEN  || 'PROVIDE TOKEN like sha256~0ITt6ksh8jakbJIxZQAspqhG5Q1m_u0zwxvdyNTTHsg';
+exports.ocpserver = process.env.OCPSERVER || 'PROFIDE OCP SERVER like https://c113-e.eu-de.containers.cloud.ibm.com:31739';
 exports.ocpingress = process.env.OCPINGRESS || 'openshift-070d31d9cf0761a13fcebd4a97861c1a-0000.eu-de.containers.appdomain.cloud'
